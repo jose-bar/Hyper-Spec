18042a1e48e453b45e3348b7d465ee8d9dc3ea35
MgMap <minaungpaing.pp@gmail.com>
2024-10-28 21:45:37 +0000

    small fix to run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -6,3 +6,4 @@ sudo docker build --no-cache -f ./docker/Dockerfile -t hyper_spec-m .
 # Run the Docker container with GPU support and dataset volume mounted
 sudo docker run --gpus all -v ~/PXD001468/:/dataset/ -it hyper_spec-m /bin/bash
 
+apt install vim
